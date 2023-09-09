02d29957a26ffa0d69a18e6f3dd7071c1048b9ef
ramos-vini <ramoscontrole@gmail.com>
2023-09-09 13:46:20 -0300

    setting up spring security

---
+++ demo/src/main/java/com/example/demo/welcome/WelcomeController.java
@@ -0,0 +1,20 @@
+package com.example.demo.welcome;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("name")
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String loginPage(@RequestParam(defaultValue = "my friend") String name, Model model){
+        model.addAttribute("name", name);
+
+        return "welcome";
+    }
+
+}
