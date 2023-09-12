6c041496401fe734c0cda343949b079791c21748
ramos-vini <ramoscontrole@gmail.com>
2023-09-12 10:47:56 -0300

    Spring Security configured to show H2 Console

--- demo/src/main/java/com/example/demo/security/SpringSecurityConfiguration.java
+++ demo/src/main/java/com/example/demo/security/SpringSecurityConfiguration.java
@@ -2,11 +2,14 @@ package com.example.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -14,12 +17,8 @@ import java.util.function.Function;
 public class SpringSecurityConfiguration {
 
     @Bean
-    public InMemoryUserDetailsManager createUserDetailsManager(){
-
-        UserDetails userDetails1 = createNewUser("vini", "mito");
-        UserDetails userDetails2 = createNewUser("nana", "banana");
-
-        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
     private UserDetails createNewUser(String username, String password){
@@ -34,7 +33,29 @@ public class SpringSecurityConfiguration {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+    public InMemoryUserDetailsManager createUserDetailsManager(){
+
+        UserDetails userDetails1 = createNewUser("vini", "mito");
+        UserDetails userDetails2 = createNewUser("nana", "banana");
+
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // All URLs are protected
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+
+        // Login Form is shown for unauthorized requests
+        http.formLogin(Customizer.withDefaults());
+
+        // CSRF is Disabled
+        http.csrf().disable();
+
+        // Frames are Disabled
+        http.headers().frameOptions().disable();
+
+        return http.build();
+    }
+
 }
